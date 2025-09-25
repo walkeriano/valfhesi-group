d929f5e98618837c4c39ea632434a32e27ad9a69
walkeriano <admin@MacBook-Pro-de-admin.local>
2025-09-25 21:55:20 +0100

    cambios estios

--- src/components/presentation/presentation.jsx
+++ src/components/presentation/presentation.jsx
@@ -6,13 +6,23 @@ export default function Presentation() {
     <section className={styles.containerPresentation}>
       <section className={styles.infoPerson}>
         <section className={styles.datosPerson}>
-          <p>
+          <h2>Sayuri Quispe</h2>
+          <h3>Abogada Fundadora</h3>
+          <h4>
             "Crear soluciones de manera eficaz a las necesidades de nuestros
             clientes y{" "}
             <span>defender sus derechos en pro del valor de la justicia"</span>
+          </h4>
+          <p>
+            Abogada fundadora con sólidos valores éticos y morales; con amplia
+            experiencia en asesoría legal en Derecho Municipal, Derecho
+            Administrativo y Gestión Pública. Maestra en Derecho de La Empresa
+            por la PUCP, Egresada de la Maestría en Derecho Penal por la UNFV.
+            Su trayectoria profesional se compone también de conocimientos
+            técnicos en el área municipal y registral, con habilidades de
+            coordinación institucional y análisis jurídico destacando en
+            asesoría y representación de empresas del sector privado.
           </p>
-          <h2>Sayuri Quispe</h2>
-          <h3>Abogada Fundadora</h3>
         </section>
       </section>
       <section className={styles.imgBox}>
